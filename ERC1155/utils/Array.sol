371d8d03296cbf3d85e8de8eee77eb064df4f073
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-06-15 20:02:47 +0800

    new content

--- ERC1155/utils/Array.sol
+++ ERC1155/utils/Array.sol
@@ -2,11 +2,20 @@
 
 pragma solidity ^0.8.20;
 
-import {Comparators} from "";
-import {SlotDerivation} from "";
-import {StorageSlot} from "";
-import {Math} from "";
+import {Comparators} from "./Comparators.sol";
+import {SlotDerivation} from "./SlotDerivation.sol";
+import {StorageSlot} from "./StorageSlot.sol";
+import {Math} from "../utils/math/Math.sol";
 
 library Arrays{
-    
+    using SlotDerivation for bytes32;
+    using StorageSlot for bytes32;
+
+    function sort(
+        uint256[] memory array,
+        function(uint256,uint256) pure returns(bool) comp
+    ) internal pure returns(uint256[] memory){
+        _quickSort(_begin(array),_end(array),comp);
+        return array;
+    }
 }
